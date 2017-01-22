f84569477e6c480bc1d40e2ce7aa7dfc5fe32098
Moshe Plotkin <mplotkin@jewpaltz.com>
2017-01-22 14:46:46 -0500

    3d hover

---
+++ Assets/Hover3D.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover3D : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    // OnMouseExit is called when the mouse is not any longer over the GUIElement or Collider
+    private void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+}
