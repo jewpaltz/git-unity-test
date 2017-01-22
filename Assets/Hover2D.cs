97c64f945f09b3f4b937597af220691bace26cbb
Moshe Plotkin <mplotkin@jewpaltz.com>
2017-01-22 14:31:19 -0500

    come clicking

---
+++ Assets/Hover2D.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover2D : MonoBehaviour {
+
+    SpriteRenderer sprite;
+
+	// Use this for initialization
+	void Start () {
+        sprite = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnMouseEnter()
+    {
+        sprite.color = Color.green;
+    }
+
+    // OnMouseExit is called when the mouse is not any longer over the GUIElement or Collider
+    private void OnMouseExit()
+    {
+        sprite.color = Color.red;
+    }
+}
---
+++ Assets/HoverUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HoverUI : MonoBehaviour {
+
+    Text textComponent;
+	// Use this for initialization
+	void Start () {
+        textComponent = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseEnter()
+    {
+        textComponent.color = Color.green;
+    }
+
+    private void OnMouseExit()
+    {
+        textComponent.color = Color.red;
+    }
+
+    private void OnMouseOver()
+    {
+
+    }
+
+
+}
