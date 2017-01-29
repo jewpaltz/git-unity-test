24df5bfebe591d661b5343c1ae90920aa83cc5e6
Moshe Plotkin <mplotkin@jewpaltz.com>
2017-01-29 10:36:14 -0500

    lots of stuff

--- Assets/Hover3D.cs
+++ Assets/Hover3D.cs
@@ -23,4 +23,34 @@ public class Hover3D : MonoBehaviour {
     {
         GetComponent<Renderer>().material.color = Color.red;
     }
+
+    bool isDragging = false;
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    private void OnMouseDown()
+    {
+        isDragging = true;
+        screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    private void OnMouseDrag()
+    {
+        if (isDragging) {
+            
+        }
+        var curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+    }
+
+    private void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+
 }
